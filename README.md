c4967677cb83551d1fc4ea99ffe8c471132602f5
spfellers <spf.github@gmail.com>
2019-11-04 21:34:32 -0600

    adding comments

---
+++ README.md
@@ -0,0 +1,3 @@
+# Testing GO api
+
+not sure if ill do anything with this but I would like a repo with testing + notes on how to write a golang API
--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"github.com/gorilla/mux"
 )
 
@@ -15,9 +16,24 @@ func getPeople(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "GOT ALL THESE PEOPLE")
 }
 
+// functions
+// start with: func
+// params can take types e.g. func f(x int)
+// specify return type e.g. func f() string { ...
+func getPort() string {
+	if args := os.Args[1:]; len(args) > 0 {
+		return args[0]
+	}
+	return "8080"
+}
+
+// use fmt.Println() for logging
 func main() {
+	// equals is :=
+	port := getPort()
 	router := mux.NewRouter().StrictSlash(true)
+	// HandleFunc(route, controller)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/people", getPeople)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":" + port, router))
 }
